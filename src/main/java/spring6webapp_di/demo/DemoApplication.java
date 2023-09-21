77b2d8be538dccc312c6ea4f0c2c5632fe26ce21
Denys Kapralov <95357917+idenyskap@users.noreply.github.com>
2023-09-21 11:43:09 +0300

    start

---
+++ src/main/java/spring6webapp_di/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package spring6webapp_di.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
